03fd403556e0568b05d204086c180347808905db
Farshid Tavakolizadeh <email@farshid.ws>
2020-06-17 11:30:17 +0200

    add extra validation, tag uri for TDs without ID

--- sync.py
+++ sync.py
@@ -1,35 +1,52 @@
 import os, json, requests, glob
 
-endpoint='http://localhost:8081/td'
-auth='Basic '
+endpoint=os.environ['ENDPOINT']
+auth=os.environ['AUTHORIZATION']
 
-url_prefix='https://github.com/w3c/wot-testing/tree/master/events/2020.06.Online/TDs'
-#tag='tag:demo.linksmart.eu,2020-06:'
+# for TD without an ID
+uri_tag_prefix='tag:demo.linksmart.eu,2020-06:'
 
 files = glob.glob('./**/*.jsonld', recursive=True)
 
+
+def put(td):
+    res = requests.put(endpoint+'/td/'+td['id'], 
+        data=json.dumps(td).encode('utf-8'), 
+        headers={'Authorization':auth})
+
+    print('Response:', res.status_code)
+    if res.status_code != 200 and res.status_code != 201:
+        print('Response body:\n', json.dumps(json.loads(res.text), indent=4))
+    return res.status_code
+
+def validate(td):
+    res = requests.get(endpoint+'/validation', 
+                data=json.dumps(td).encode('utf-8'), 
+                headers={'Authorization':auth})
+    print(json.dumps(json.loads(res.text), indent=4))
+
 for filename in files:
-    print('\nFile:', filename)
+    print('File:', filename)
     with open(filename) as f:
         read_data = f.read()
         td = json.loads(read_data)
         if 'id' not in td:
-            print("TD has no ID")
+            print("TD has no ID. Construct a Tag URI with filename:")
             id=filename
-            if id.startswith("."):
-                id=id[1:]
-            # if id.endswith(".jsonld"):
-            #     id=id[2:]
-            td['id']=url_prefix+id
+            if id.startswith("./"):
+                id=id[2:]
+            if id.endswith(".jsonld"):
+                id=id[:-7]
+            td['id']=uri_tag_prefix+id
             
         print('ID:', td['id'])
         # Submit
-        continue
         try:
-            res = requests.put(endpoint+'/'+td['id'], 
-                data=json.dumps(td).encode('utf-8'), 
-                headers={'Authorization':auth})
-            print('Response:', res.status_code, res.text)
+            code = put(td)
+            if code == 400:
+                print('Validate again:')
+                validate(td)
         except requests.exceptions.RequestException as e:
             raise SystemExit(e)
-            
+    
+    print('----------\n')
