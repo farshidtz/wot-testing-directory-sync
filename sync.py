d2b3f045a89721f2d717a21f10c39e92d26fa9df
Farshid Tavakolizadeh <email@farshid.ws>
2020-06-17 16:47:49 +0200

    post TDs which have no ID

--- sync.py
+++ sync.py
@@ -3,11 +3,10 @@ import os, json, requests, glob
 ENDPOINT=os.environ['ENDPOINT'] # directory root endpoint
 AUTH=os.environ['AUTHORIZATION'] # authorization header
 TTL=os.environ['TTL'] # seconds
-URI_PREFIX=os.environ['URI_PREFIX'] # for TDs without an ID
 
 files = glob.glob('./**/*.jsonld', recursive=True)
 
-# add a TD or update an existing one
+# create a TD or update an existing one
 def put(td):
     url = ENDPOINT+'/td/'+td['id']
     print('PUT', url)
@@ -19,6 +18,34 @@ def put(td):
     
     return res.status_code
 
+# create a TD
+def post(td):
+    url = ENDPOINT+'/td'
+    print('POST', url)
+    res = requests.post(url, data=json.dumps(td).encode('utf-8'), headers={'Authorization':AUTH})
+    print('Response:', res.status_code, res.reason)
+    if res.status_code == 201:
+        print('Generated ID:', res.headers['Location'])
+
+    if res.text != "":
+        print('Response body:\n', json.dumps(json.loads(res.text), indent=4))
+    
+    return res.status_code
+
+def submit(td):
+    if 'id' in td:
+        print('ID:', td['id'])
+        code = put(td)
+    else:
+        print("--> TD has no ID. Will POST.")
+        code = post(td)
+
+    
+    if code == 400:
+        print('\nValidate the TD explicitly:')
+        validate(td)
+
+
 # validate a TD
 def validate(td):
     url = ENDPOINT+'/validation'
@@ -26,31 +53,17 @@ def validate(td):
     res = requests.get(url, data=json.dumps(td).encode('utf-8'), headers={'Authorization':AUTH})
     print('Validation results:\n', json.dumps(json.loads(res.text), indent=4))
 
+
 for filename in files:
     print('File:', filename)
     with open(filename) as f:
         td = json.loads(f.read())
 
-        if 'id' not in td:
-            print("--> TD has no ID. Construct a Tag URI with filename:")
-            id=filename
-            if id.startswith("./"):
-                id=id[2:]
-            if id.endswith(".jsonld"):
-                id=id[:-7]
-            td['id']=URI_PREFIX+id
-            
-        print('ID:', td['id'])
-
         # inject ttl
         td['ttl']=int(TTL)
 
-        # Submit
         try:
-            code = put(td)
-            if code == 400:
-                print('\nValidate the TD explicitly:')
-                validate(td)
+            submit(td)
         except requests.exceptions.RequestException as e:
             raise SystemExit(e)
     
